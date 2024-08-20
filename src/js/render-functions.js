489ac0a3264730e6936f41f7fc4721e384ee54e3
Eugene Kuz <tir26@ukr.net>
2024-08-20 15:09:44 +0300

    add simplelightbox to images. add description under image

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,4 +1,5 @@
 import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
 import { fetchPhotos } from './pixabay-api.js';
 
 const searchFormEl = document.querySelector('.form');
@@ -7,28 +8,39 @@ const galleryListEl = document.querySelector('.gallery-list');
 const crateGalleyCardTemplate = imgInfo => {
   return `
     <li class = "gallery-card">
-      <div class=image-container>
-          <img class ="gallery-image" src="${imgInfo.webformatURL}" alt ="${imgInfo.tags}"/>
+      <div class="image-container">
+          <a href="${imgInfo.largeImageURL}" class="original-image-link">
+            <img class ="gallery-image" src="${imgInfo.webformatURL}" alt ="${imgInfo.tags}"/>
+          </a>
+          <div class ="gallery-image-info-container">
             <div class = "gallery-image-info">
-              <p class ="gallery-image-popularity">Likes</p>
-              <p>Number</p>
-               
+                <p class ="gallery-text">Likes</p>
+                <p class = "gallery-value">${imgInfo.likes}</p>
             </div>
             <div class = "gallery-image-info">
-              <p class ="gallery-image-popularity">Likes</p>
-                 <p>Number</p>
+              <p class ="gallery-text">Views</p>
+              <p class = "gallery-value">${imgInfo.views}</p>
             </div>
             <div class = "gallery-image-info">
-              <p class ="gallery-image-popularity">Likes</p>
-                 <p>Number</p>
+              <p class ="gallery-text">Comments</p>
+              <p class = "gallery-value">${imgInfo.comments}</p>
             </div>
             <div class = "gallery-image-info">
-              <p class ="gallery-image-popularity">Likes</p>
-                 <p>Number</p>
+              <p class ="gallery-text">Downloads</p>
+              <p class = "gallery-value">${imgInfo.downloads}</p>
             </div>
+          </div>
       </div>
     </li>
     `;
+};
+
+const createSimpleLightBox = () => {
+  return new SimpleLightbox('.image-container a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.8,
+  });
 };
 
 const onSearchFormSubmit = event => {
@@ -51,6 +63,7 @@ const onSearchFormSubmit = event => {
         .map(image => crateGalleyCardTemplate(image))
         .join('');
       galleryListEl.insertAdjacentHTML('beforeend', galleyCardTemplate);
+      createSimpleLightBox();
     })
     .catch(error => {
       console.log(error);
