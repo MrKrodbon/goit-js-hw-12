f66d13793c78f7e431f7011e13c8f84a918a9398
Eugene Kuz <tir26@ukr.net>
2024-08-21 14:51:39 +0300

    fix loader and move code to main.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,14 +1,6 @@
-import iziToast from 'izitoast';
-import SimpleLightbox from 'simplelightbox';
-import { fetchPhotos } from './pixabay-api.js';
 
-const searchFormEl = document.querySelector('.form');
-const galleryListEl = document.querySelector('.gallery-list');
-const createLoaderTemplate = () => {
-  return `<div class="loader"></div>`;
-};
 
-const crateGalleyCardTemplate = imgInfo => {
+export const crateGalleyCardTemplate = imgInfo => {
   return `
     <li class = "gallery-card">
       <div class="image-container">
@@ -38,47 +30,4 @@ const crateGalleyCardTemplate = imgInfo => {
     `;
 };
 
-const createSimpleLightBox = () => {
-  return new SimpleLightbox('.image-container a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    overlayOpacity: 0.8,
-  });
-};
 
-const onSearchFormSubmit = event => {
-  let loader;
-  galleryListEl.innerHTML = '';
-  const searchedValue = searchFormEl.elements.user_query.value;
-  loader = createLoader();
-  galleryListEl.innerHTML = loader;
-  let divLoader = document.querySelector('.loader');
-  fetchPhotos(searchedValue)
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          title: 'Error',
-          message:
-            'Sorry,there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-        galleryListEl.innerHTML = '';
-        searchFormEl.reset();
-        return;
-      }
-
-      divLoader.classList.add('visually-hidden');
-      const galleyCardTemplate = data.hits
-        .map(image => crateGalleyCardTemplate(image))
-        .join('');
-      galleryListEl.insertAdjacentHTML('beforeend', galleyCardTemplate);
-      createSimpleLightBox();
-    })
-    .catch(error => {
-      
-      console.log(error);
-    });
-  event.preventDefault();
-};
-
-searchFormEl.addEventListener('submit', onSearchFormSubmit);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,63 @@
+import { crateGalleyCardTemplate } from './js/render-functions.js';
+import { fetchPhotos } from './js/pixabay-api.js';
+import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
 
+const searchFormEl = document.querySelector('.form');
+const galleryListEl = document.querySelector('.gallery-list');
+const createSimpleLightBox = () => {
+  return new SimpleLightbox('.image-container a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.8,
+  });
+};
+const lightBox = createSimpleLightBox();
 
+const onSearchFormSubmit = event => {
+  galleryListEl.innerHTML = '';
+  const searchedValue = searchFormEl.elements.user_query.value;
 
+  if (searchedValue === '') {
+    event.preventDefault();
+    iziToast.error({
+      title: 'Error',
+      message: 'Please fill input field',
+      position: 'topRight',
+    });
+    return;
+  } else {
+    galleryListEl.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="loader"></div>`
+    );
+    let divLoader = document.querySelector('.loader');
+    fetchPhotos(searchedValue)
+      .then(data => {
+        if (data.hits.length === 0) {
+          iziToast.error({
+            title: 'Error',
+            message:
+              'Sorry,there are no images matching your search query. Please try again!',
+            position: 'topRight',
+          });
+          galleryListEl.innerHTML = '';
+          searchFormEl.reset();
+          return;
+        }
+        divLoader.classList.add('visually-hidden');
+        const galleyCardTemplate = data.hits
+          .map(image => crateGalleyCardTemplate(image))
+          .join('');
+        galleryListEl.insertAdjacentHTML('afterbegin', galleyCardTemplate);
+        lightBox.refresh();
+      })
+      .catch(error => {
+        console.log(error.status);
+      });
+  }
+
+  event.preventDefault();
+};
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
