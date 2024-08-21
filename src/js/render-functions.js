fcd7100d833d415b6877c8e07e21f54a0975871f
Eugene Kuz <tir26@ukr.net>
2024-08-21 09:53:24 +0300

    fix loader

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,9 +1,14 @@
-import SimpleLightbox from 'simplelightbox';
 import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
+import { fetchPhotos } from './pixabay-api.js';
+
 const searchFormEl = document.querySelector('.form');
 const galleryListEl = document.querySelector('.gallery-list');
+const createLoaderTemplate = () => {
+  return `<div class="loader"></div>`;
+};
 
-export const crateGalleyCardTemplate = imgInfo => {
+const crateGalleyCardTemplate = imgInfo => {
   return `
     <li class = "gallery-card">
       <div class="image-container">
@@ -33,16 +38,21 @@ export const crateGalleyCardTemplate = imgInfo => {
     `;
 };
 
-export const createSimpleLightBox = () => {
+const createSimpleLightBox = () => {
   return new SimpleLightbox('.image-container a', {
     captionsData: 'alt',
     captionDelay: 250,
     overlayOpacity: 0.8,
   });
 };
+
 const onSearchFormSubmit = event => {
+  let loader;
   galleryListEl.innerHTML = '';
   const searchedValue = searchFormEl.elements.user_query.value;
+  loader = createLoader();
+  galleryListEl.innerHTML = loader;
+  let divLoader = document.querySelector('.loader');
   fetchPhotos(searchedValue)
     .then(data => {
       if (data.hits.length === 0) {
@@ -56,6 +66,8 @@ const onSearchFormSubmit = event => {
         searchFormEl.reset();
         return;
       }
+
+      divLoader.classList.add('visually-hidden');
       const galleyCardTemplate = data.hits
         .map(image => crateGalleyCardTemplate(image))
         .join('');
@@ -63,8 +75,10 @@ const onSearchFormSubmit = event => {
       createSimpleLightBox();
     })
     .catch(error => {
+      
       console.log(error);
     });
   event.preventDefault();
 };
+
 searchFormEl.addEventListener('submit', onSearchFormSubmit);
