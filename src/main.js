394912ceeb8f75493b57eddb13138e59883c2ad7
Eugene Kuz <tir26@ukr.net>
2024-08-30 16:43:41 +0300

    fix issues with scroll

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@ import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 import { getPhotos } from './js/pixabay-api.js';
 
-const _searchFormEl = document.querySelector('.form');
-const _serachedInput = document.querySelector('.form input');
-const _galleryListEl = document.querySelector('.gallery-list');
-const _mainLoader = document.querySelector('.main-load-wrapper .loader');
-const _loadMoreBtn = document.querySelector('.loadMoreBtn');
-const _loadMoreWrapperLoader = document.querySelector(
+const searchFormEl = document.querySelector('.form');
+const serachedInput = document.querySelector('.form input');
+const galleryListEl = document.querySelector('.gallery-list');
+const mainLoader = document.querySelector('.main-load-wrapper .loader');
+const loadMoreBtn = document.querySelector('.loadMoreBtn');
+const loadMoreWrapperLoader = document.querySelector(
   '.load-more-wrapper .loader'
 );
 
-let _currentPage = 1;
+let currentPage = 1;
 let searchedValue;
+let totalHits;
+let responseHitsArray;
 
 const createSimpleLightBox = () => {
   return new SimpleLightbox('.image-container a', {
@@ -28,104 +30,111 @@ const lightBox = createSimpleLightBox();
 const renderPhotos = async event => {
   event.preventDefault();
 
-  _galleryListEl.innerHTML = '';
-  searchedValue = _searchFormEl.elements.user_query.value.trim();
+  galleryListEl.innerHTML = '';
+  searchedValue = searchFormEl.elements.user_query.value.trim();
+  currentPage = 1;
 
-  try {
-    _mainLoader.classList.remove('visually-hidden');
-    _loadMoreBtn.classList.add('visually-hidden');
-    const response = await getPhotos(searchedValue, _currentPage);
-    _searchFormEl.reset();
-    _mainLoader.classList.add('visually-hidden');
-    let responseDataArray = response.data.hits;
-    if (searchedValue === '') {
-      iziToast.error({
-        title: 'Error',
-        message: 'Please fill input field',
-        position: 'topRight',
-      });
-      return;
-    }
-    if (responseDataArray.length === 0) {
-      iziToast.error({
-        title: 'Error',
-        message:
-          'Sorry,there are no images matching your search query. Please try again!',
-        position: 'topRight',
-      });
-      _loadMoreBtn.classList.add('visually-hidden');
-      _galleryListEl.innerHTML = '';
-      _searchFormEl.reset();
-      return;
-    }
-
-    const galleryCardTemplate = responseDataArray
-      .map(image => crateGalleryCardTemplate(image))
-      .join('');
-    _galleryListEl.insertAdjacentHTML('afterbegin', galleryCardTemplate);
-
-    lightBox.refresh();
-    _loadMoreBtn.classList.remove('visually-hidden');
-  } catch (error) {
+  if (searchedValue === '') {
     iziToast.error({
       title: 'Error',
-      message: 'Please check your internet connection and try again!',
+      message: 'Please fill input field',
       position: 'topRight',
     });
-    _serachedInput.textContent = '';
-    _mainLoader.classList.add('visually-hidden');
-    _loadMoreBtn.classList.add('visually-hidden');
+    return;
+  } else {
+    try {
+      mainLoader.classList.remove('visually-hidden');
+      loadMoreBtn.classList.add('visually-hidden');
+      const response = await getPhotos(searchedValue, currentPage);
+      totalHits = response.data.totalHits;
+      searchFormEl.reset();
+      mainLoader.classList.add('visually-hidden');
+      responseHitsArray = response.data.hits;
+
+      if (responseHitsArray.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry,there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        loadMoreBtn.classList.add('visually-hidden');
+        galleryListEl.innerHTML = '';
+        searchFormEl.reset();
+        return;
+      }
+
+      if (responseHitsArray.length < 15) {
+        loadMoreBtn.classList.add('visually-hidden');
+      } else {
+        loadMoreBtn.classList.remove('visually-hidden');
+      }
+      const galleryCardTemplate = responseHitsArray
+        .map(image => crateGalleryCardTemplate(image))
+        .join('');
+      galleryListEl.insertAdjacentHTML('afterbegin', galleryCardTemplate);
+
+      lightBox.refresh();
+    } catch (error) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please check your internet connection and try again!',
+        position: 'topRight',
+      });
+      serachedInput.textContent = '';
+      mainLoader.classList.add('visually-hidden');
+      loadMoreBtn.classList.add('visually-hidden');
+    }
   }
 };
 
 const onLoadMoreBtn = async () => {
-  const firstImageElement = document.querySelector('.gallery-list li');
   try {
-    _currentPage++;
-    _loadMoreBtn.classList.add('visually-hidden');
-    _loadMoreWrapperLoader.classList.remove('visually-hidden');
-    const response = await getPhotos(searchedValue, _currentPage);
-
-    let responseHitsArray = response.data.hits;
-    let responseTotalHits = response.data.totalHits;
-    let totalPages = Math.ceil(responseTotalHits / responseHitsArray.length);
-
-    if (_currentPage >= totalPages) {
+    currentPage++;
+    loadMoreBtn.classList.add('visually-hidden');
+    loadMoreWrapperLoader.classList.remove('visually-hidden');
+    const response = await getPhotos(searchedValue, currentPage);
+    loadMoreBtn.classList.remove('visually-hidden');
+    loadMoreWrapperLoader.classList.add('visually-hidden');
+    responseHitsArray = response.data.hits;
+    totalHits = response.data.totalHits;
+    if (currentPage * 15 >= totalHits) {
       iziToast.info({
         title: 'info',
         message: "We're sorry, but you've reached the end of search results.",
         position: 'topRight',
       });
-      _loadMoreWrapperLoader.classList.add('visually-hidden');
-      _loadMoreBtn.classList.add('visually-hidden');
-      return;
-    } else {
-      const galleryCardTemplate = responseHitsArray
-        .map(image => crateGalleryCardTemplate(image))
-        .join('');
-      _galleryListEl.insertAdjacentHTML('beforeend', galleryCardTemplate);
-
-      _loadMoreWrapperLoader.classList.add('visually-hidden');
-      _loadMoreBtn.classList.remove('visually-hidden');
-
-      let boundingRect = firstImageElement.getBoundingClientRect();
-      window.scrollBy({
-        top: boundingRect.top * boundingRect.y,
-        behavior: 'smooth',
-      });
+      loadMoreWrapperLoader.classList.add('visually-hidden');
+      loadMoreBtn.classList.add('visually-hidden');
     }
+
+    const galleryCardTemplate = responseHitsArray
+      .map(image => crateGalleryCardTemplate(image))
+      .join('');
+    galleryListEl.insertAdjacentHTML('beforeend', galleryCardTemplate);
+
+    makeScroll();
   } catch (error) {
     iziToast.error({
       title: 'Error',
       message: 'Please check your internet connection and try again!',
       position: 'topRight',
     });
-    _searchFormEl.elements.user_query.value = '';
-    _loadMoreWrapperLoader.classList.add('visually-hidden');
-    _loadMoreBtn.classList.remove('visually-hidden');
+    searchFormEl.elements.user_query.value = '';
+    loadMoreWrapperLoader.classList.add('visually-hidden');
+    loadMoreBtn.classList.remove('visually-hidden');
   }
 };
 
-_searchFormEl.addEventListener('submit', renderPhotos);
+function makeScroll() {
+  const firstImageElement = document.querySelector('.gallery-list li');
+  let boundingRect = firstImageElement.getBoundingClientRect();
+  window.scrollBy({
+    top: boundingRect.height * 2,
+    behavior: 'smooth',
+  });
+}
 
-_loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+searchFormEl.addEventListener('submit', renderPhotos);
+
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
